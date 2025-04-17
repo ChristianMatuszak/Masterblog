089da4088aea204dfef9d20851bd7161bafe2aba
ChristianMatuszak <c.matuszak@outlook.com>
2025-04-17 11:03:27 +0200

    feat: template and style
    - Added folder static and templates
    - Added index.html and style.css
    - Added post.json with 2 examples

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def load_posts():
+    with open("post.json", "r") as file:
+        return json.load(file)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template("index.html", posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
